7d9e05498aa76579eb208f7925c21add2202a5e8
kian-404 <tianjianen2015@gmail.com>
2026-06-25 19:32:02 +0800

    feat: 更新应用 ID 和添加 @capacitor/assets 依赖

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'kianer.app.reader',
   appName: 'Kianer',
   webDir: 'dist'
 };
